bb0f9b418649a8a3910e1e6e880743e904c9344e
simon <slacis@student.unimelb.edu.au>
2016-05-25 10:26:12 +0900

    fixed error in contacts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'https://ancient-lake-61393.herokuapp.com/' }
-  config.action_mailer.deliver_method = :smtp
+  config.action_mailer.delivery_method = :smtp
 
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
